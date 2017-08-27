3c022238bf5f400d4129e69b4b78efb30a89782b
Justin Connell <justin.connell@idg-corp.com>
2017-08-26 21:10:23 -0400

    Add project files.

---
+++ Engine/Armor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Armor : Item
+    {
+        public int MinimumArmor { get; set; }
+        public int MaximumArmor { get; set; }
+
+        public Armor(int id, string name, string namePlural, int minimumArmor, int maximumArmor) : base(id, name, namePlural)
+        {
+            MinimumArmor = minimumArmor;
+            MaximumArmor = maximumArmor;
+        }
+    }
+}
---
+++ Engine/NPC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class NPC : Creature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public NPC(int id, string name, int currentHitPoints, int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
